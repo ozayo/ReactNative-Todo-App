2a868ac059f312d18bb25ed30b0a7e0f7663c377
ozayo <ozay.ozdemir@gmail.com>
2024-06-03 17:34:13 +0200

    remove navigation on home screen

--- App.js
+++ App.js
@@ -1,26 +1,11 @@
-// App.js
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
 import { TodosProvider } from './context/TodosContext';
-import HomeScreen from './screens/HomeScreen';
-import DetailScreen from './screens/DetailScreen';
-import AddScreen from './screens/AddScreen';
-import EditTaskScreen from './screens/EditTaskScreen';
-
-const Stack = createNativeStackNavigator();
+import StackNavigator from './navigation/StackNavigator';
 
 export default function App() {
   return (
     <TodosProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Detail" component={DetailScreen} />
-          <Stack.Screen name="Add" component={AddScreen} />
-          <Stack.Screen name="EditTask" component={EditTaskScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <StackNavigator />
     </TodosProvider>
   );
 }
