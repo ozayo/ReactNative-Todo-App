15113c5e0ddc6362a532ab359b1fdd344fe5132a
ozayo <ozay.ozdemir@gmail.com>
2024-06-03 17:37:11 +0200

    add icons

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,5 @@
-// screens/HomeScreen.js
 import React, { useContext } from 'react';
-import { View, Text, Button, SectionList, TouchableOpacity } from 'react-native';
+import { View, Text, SectionList, TouchableOpacity, Image } from 'react-native';
 import { globalStyles } from '../styles/globalStyles';
 import { TodosContext } from '../context/TodosContext';
 
@@ -11,25 +10,31 @@ export default function HomeScreen({ navigation }) {
   const pendingTodos = todos.filter(todo => !todo.completed);
 
   const sections = [
-    { title: 'Todos', data: pendingTodos },
-    { title: 'Genomfört', data: completedTodos }
+    { title: 'Tasks', data: pendingTodos },
+    { title: 'Completed', data: completedTodos }
   ];
 
   return (
     <View style={globalStyles.container}>
+      <Text style={globalStyles.h1}>My Todos</Text>
       <SectionList
         sections={sections}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
           <TouchableOpacity onPress={() => navigation.navigate('Detail', { todo: item })}>
-            <Text>{item.title}</Text>
+            <View style={globalStyles.itemContainer}>
+              <Image 
+                source={item.completed ? require('../assets/check.png') : require('../assets/uncheck.png')} 
+                style={globalStyles.itemImage} 
+              />
+              <Text style={globalStyles.itemTitle}>{item.title}</Text>
+            </View>
           </TouchableOpacity>
         )}
         renderSectionHeader={({ section: { title } }) => (
-          <Text>{title}</Text>
+          <Text style={globalStyles.h2}>{title}</Text>
         )}
       />
-      <Button title="Add" onPress={() => navigation.navigate('Add')} />
     </View>
   );
 }
